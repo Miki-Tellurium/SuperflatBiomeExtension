c7456f563bf36f56426c73ed054398289a0f4482
Miki_Tellurium <michelegamer14@gmail.com>
2025-05-24 19:08:46 +0200

    Removed mixins

--- src/main/java/com/mikitellurium/superflatbiomeextension/mixin/SinglePoolElementMixin.java
+++
@@ -1,9 +0,0 @@
-package com.mikitellurium.superflatbiomeextension.mixin;
-
-import net.minecraft.structure.pool.SinglePoolElement;
-import org.spongepowered.asm.mixin.Mixin;
-
-@Mixin(SinglePoolElement.class)
-public class SinglePoolElementMixin {
-
-}
