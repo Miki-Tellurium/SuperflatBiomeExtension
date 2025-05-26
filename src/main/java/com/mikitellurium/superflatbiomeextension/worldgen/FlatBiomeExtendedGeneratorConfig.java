0f6f9e4429fdf204f3a489a3388359481f96dc35
Miki_Tellurium <michelegamer14@gmail.com>
2025-05-26 16:31:33 +0200

    Refactored feature check getters

--- src/main/java/com/mikitellurium/superflatbiomeextension/worldgen/FlatBiomeExtendedGeneratorConfig.java
+++ src/main/java/com/mikitellurium/superflatbiomeextension/worldgen/FlatBiomeExtendedGeneratorConfig.java
@@ -5,7 +5,10 @@ import com.mikitellurium.superflatbiomeextension.mixin.GenerationShapeConfigAcce
 import com.mojang.serialization.Codec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
 import net.minecraft.block.Blocks;
-import net.minecraft.registry.*;
+import net.minecraft.registry.BuiltinRegistries;
+import net.minecraft.registry.RegistryEntryLookup;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.registry.RegistryWrapper;
 import net.minecraft.registry.entry.RegistryEntry;
 import net.minecraft.registry.entry.RegistryEntryList;
 import net.minecraft.util.math.noise.DoublePerlinNoiseSampler;
@@ -15,7 +18,7 @@ import net.minecraft.world.biome.source.util.VanillaBiomeParameters;
 import net.minecraft.world.gen.GenerationStep;
 import net.minecraft.world.gen.chunk.ChunkGeneratorSettings;
 import net.minecraft.world.gen.densityfunction.DensityFunction;
-import net.minecraft.world.gen.feature.*;
+import net.minecraft.world.gen.feature.PlacedFeature;
 
 import java.util.List;
 import java.util.Map;
@@ -26,41 +29,32 @@ public class FlatBiomeExtendedGeneratorConfig {
             (instance) -> instance.group(
                     Codec.INT.fieldOf("world_height").forGetter((config) -> config.height),
                     Codec.BOOL.fieldOf("generate_water").forGetter((config) -> config.generateWater),
-                    Codec.BOOL.fieldOf("has_lava_lakes").forGetter((config) -> config.lavaLakes),
-                    Codec.BOOL.fieldOf("generate_ores").forGetter((config) -> config.generateOres),
                     Codec.BOOL.fieldOf("has_features").forGetter((config) -> config.hasFeatures),
-                    Codec.BOOL.fieldOf("has_structures").forGetter((config) -> config.hasStructures),
-                    RegistryOps.getEntryCodec(MiscPlacedFeatures.DESERT_WELL)
+                    Codec.BOOL.fieldOf("has_lava_lakes").forGetter(FlatBiomeExtendedGeneratorConfig::hasLavaLakes),
+                    Codec.BOOL.fieldOf("generate_ores").forGetter(FlatBiomeExtendedGeneratorConfig::generateOres),
+                    Codec.BOOL.fieldOf("has_structures").forGetter(FlatBiomeExtendedGeneratorConfig::hasStructures)
             ).apply(instance, FlatBiomeExtendedGeneratorConfig::new)
     );
-    private final ChunkGeneratorSettings settings;
     private final int height;
     private final int surfaceY;
     private final boolean generateWater;
-    private final boolean lavaLakes;
-    private final boolean generateOres;
     private final boolean hasFeatures;
-    private final boolean hasStructures;
     private final Map<Integer, FeatureStepCheck> featureChecks;
-    private final RegistryEntry<PlacedFeature> feature;
+    private final ChunkGeneratorSettings settings;
 
-    public FlatBiomeExtendedGeneratorConfig(int height, boolean generateWater, boolean lavaLakes, boolean generateOres, boolean hasFeatures, boolean hasStructures, RegistryEntry<PlacedFeature> feature) {
+    public FlatBiomeExtendedGeneratorConfig(int height, boolean generateWater, boolean hasFeatures, boolean lavaLakes, boolean generateOres, boolean hasStructures) {
         this.height = height;
         this.surfaceY = -64 + height;
         this.generateWater = generateWater;
-        this.lavaLakes = lavaLakes;
-        this.generateOres = generateOres;
         this.hasFeatures = hasFeatures;
-        this.hasStructures = hasStructures;
-        this.feature = feature;
-        this.settings = this.createDefaultSettings();
         this.featureChecks = Map.of(
-                GenerationStep.Feature.LAKES.ordinal(), new FeatureStepCheck(this.lavaLakes, GenerationStep.Feature.LAKES),
-                GenerationStep.Feature.UNDERGROUND_ORES.ordinal(), new FeatureStepCheck(this.generateOres, GenerationStep.Feature.UNDERGROUND_ORES),
-                GenerationStep.Feature.UNDERGROUND_STRUCTURES.ordinal(), new FeatureStepCheck(this.hasStructures, GenerationStep.Feature.UNDERGROUND_STRUCTURES),
-                GenerationStep.Feature.SURFACE_STRUCTURES.ordinal(), new FeatureStepCheck(this.hasStructures, GenerationStep.Feature.SURFACE_STRUCTURES),
-                GenerationStep.Feature.STRONGHOLDS.ordinal(), new FeatureStepCheck(this.hasStructures, GenerationStep.Feature.STRONGHOLDS)
+                GenerationStep.Feature.LAKES.ordinal(), new FeatureStepCheck(lavaLakes, GenerationStep.Feature.LAKES),
+                GenerationStep.Feature.UNDERGROUND_ORES.ordinal(), new FeatureStepCheck(generateOres, GenerationStep.Feature.UNDERGROUND_ORES),
+                GenerationStep.Feature.UNDERGROUND_STRUCTURES.ordinal(), new FeatureStepCheck(hasStructures, GenerationStep.Feature.UNDERGROUND_STRUCTURES),
+                GenerationStep.Feature.SURFACE_STRUCTURES.ordinal(), new FeatureStepCheck(hasStructures, GenerationStep.Feature.SURFACE_STRUCTURES),
+                GenerationStep.Feature.STRONGHOLDS.ordinal(), new FeatureStepCheck(hasStructures, GenerationStep.Feature.STRONGHOLDS)
         );
+        this.settings = this.createDefaultSettings();
     }
 
     public ChunkGeneratorSettings getSettings() {
@@ -72,7 +66,7 @@ public class FlatBiomeExtendedGeneratorConfig {
         GenerationSettings generationSettings = biomeEntry.value().getGenerationSettings();
         List<RegistryEntryList<PlacedFeature>> list = generationSettings.getFeatures();
         for (int i = 0; i < list.size(); i++) {
-            if ((this.hasFeatures && !featureChecks.containsKey(i)) || this.featureChecks.get(i).test(i))
+            if ((this.hasFeatures() && !featureChecks.containsKey(i)) || this.featureChecks.get(i).test(i))
                 for (RegistryEntry<PlacedFeature> feature : list.get(i)) {
                     builder.addFeature(i, feature);
                 }
@@ -89,7 +83,7 @@ public class FlatBiomeExtendedGeneratorConfig {
                 Blocks.STONE.getDefaultState(),
                 Blocks.WATER.getDefaultState(),
                 DensityFunctionsAccessor.invokeCreateSurfaceNoiseRouter(densityFunctionRegistry, noiseParametersRegistry, false, false),
-                ModSurfaceRules.createDefaultModSurfaceRule(this.surfaceY, this.generateWater),
+                ModSurfaceRules.createDefaultModSurfaceRule(this.surfaceY, this.generateWater()),
                 new VanillaBiomeParameters().getSpawnSuitabilityNoises(),
                 this.surfaceY - 1,
                 false,
@@ -104,29 +98,31 @@ public class FlatBiomeExtendedGeneratorConfig {
     }
 
     public boolean generateWater() {
-        return generateWater;
-    }
-
-    public boolean hasLavaLakes() {
-        return lavaLakes;
-    }
-
-    public boolean generateOres() {
-        return generateOres;
+        return this.generateWater;
     }
 
     public boolean hasFeatures() {
-        return hasFeatures;
+        return this.hasFeatures;
+    }
+
+    public boolean hasLavaLakes() {
+        return featureChecks.get(GenerationStep.Feature.LAKES.ordinal()).isEnabled();
+    }
+
+    public boolean generateOres() {
+        return featureChecks.get(GenerationStep.Feature.UNDERGROUND_ORES.ordinal()).isEnabled();
     }
 
     public boolean hasStructures() {
-        return hasStructures;
+        return featureChecks.get(GenerationStep.Feature.UNDERGROUND_STRUCTURES.ordinal()).isEnabled() ||
+                featureChecks.get(GenerationStep.Feature.SURFACE_STRUCTURES.ordinal()).isEnabled() ||
+                featureChecks.get(GenerationStep.Feature.STRONGHOLDS.ordinal()).isEnabled();
     }
 
-    private record FeatureStepCheck(Boolean bool, GenerationStep.Feature featureStep) implements Predicate<Integer> {
+    private record FeatureStepCheck(boolean isEnabled, GenerationStep.Feature featureStep) implements Predicate<Integer> {
         @Override
         public boolean test(Integer i) {
-            return bool && i == featureStep.ordinal();
+            return isEnabled && i == featureStep.ordinal();
         }
     }
 }
