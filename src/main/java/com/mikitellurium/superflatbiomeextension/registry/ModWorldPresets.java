f7d9b7f1f14e09bcde11e9ed91a20fe360ff3680
Miki_Tellurium <michelegamer14@gmail.com>
2025-05-29 11:30:37 +0200

    Fixed key not being final

--- src/main/java/com/mikitellurium/superflatbiomeextension/registry/ModWorldPresets.java
+++ src/main/java/com/mikitellurium/superflatbiomeextension/registry/ModWorldPresets.java
@@ -6,7 +6,7 @@ import net.minecraft.util.Identifier;
 import net.minecraft.world.gen.WorldPreset;
 
 public class ModWorldPresets {
-    public static RegistryKey<WorldPreset> FLAT_BIOME_EXTENDED = of("flat_biome_extended");
+    public static final RegistryKey<WorldPreset> FLAT_BIOME_EXTENDED = of("flat_biome_extended");
 
     private static RegistryKey<WorldPreset> of(String id) {
         return RegistryKey.of(RegistryKeys.WORLD_PRESET, Identifier.ofVanilla(id));
