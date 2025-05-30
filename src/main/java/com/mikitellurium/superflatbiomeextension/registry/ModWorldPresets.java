c362a1c6b35587b490814faa60c9e4ee5884b45a
Miki_Tellurium <michelegamer14@gmail.com>
2025-05-30 12:47:32 +0200

    Fixed identifier

--- src/main/java/com/mikitellurium/superflatbiomeextension/registry/ModWorldPresets.java
+++ src/main/java/com/mikitellurium/superflatbiomeextension/registry/ModWorldPresets.java
@@ -1,5 +1,6 @@
 package com.mikitellurium.superflatbiomeextension.registry;
 
+import com.mikitellurium.superflatbiomeextension.util.FastId;
 import net.minecraft.registry.RegistryKey;
 import net.minecraft.registry.RegistryKeys;
 import net.minecraft.util.Identifier;
@@ -9,6 +10,6 @@ public class ModWorldPresets {
     public static final RegistryKey<WorldPreset> FLAT_BIOME_EXTENDED = of("flat_biome_extended");
 
     private static RegistryKey<WorldPreset> of(String id) {
-        return RegistryKey.of(RegistryKeys.WORLD_PRESET, Identifier.ofVanilla(id));
+        return RegistryKey.of(RegistryKeys.WORLD_PRESET, FastId.ofMod(id));
     }
 }
